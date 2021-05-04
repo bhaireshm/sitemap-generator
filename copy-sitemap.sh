73d948513c0f19af09ac95d340a5feaf3cf31bad
Bhairesh <bhairesh@arithaconsulting.com>
2021-05-04 13:12:07 +0530

    Update: Copy command.

--- copy-sitemap.sh
+++ copy-sitemap.sh
@@ -1,7 +1,5 @@
 # Copy Updated Jobs XML file to 1aJob folder
-# chmod 777 /opt/sitemap/jobs-sitemap.xml
-# chmod 777 /var/www/wannajob/src/sitemaps/jobs-sitemap.xml
-cp -f /opt/sitemap/jobs-sitemap.xml /var/www/wannajob/src/sitemaps/jobs-sitemap.xml
+cp /opt/sitemap/jobs-sitemap.xml /var/www/wannajob/sitemaps/jobs-sitemap.xml
 
 # Copy Updated Companies XML file to 1aJob folder 
-# cp -f /var/opt/sitemap-creator/companies-sitemap.xml /var/opt/wannajob/src/sitemaps/companies-sitemap.xml
+# cp -f /var/opt/sitemap-creator/companies-sitemap.xml /var/opt/wannajob/sitemaps/companies-sitemap.xml
